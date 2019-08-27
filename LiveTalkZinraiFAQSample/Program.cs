8154ad69fd71f7cca9655671524a68ca55ce8c21
FUJITSU Software LiveTalk <47411194+FujitsuSSL-LiveTalk@users.noreply.github.com>
2019-08-27 12:07:49 +0900

    Add files via upload

---
+++ LiveTalkZinraiFAQSample/Program.cs
@@ -0,0 +1,135 @@
+﻿/*
+ * Copyright 2019 FUJITSU SOCIAL SCIENCE LABORATORY LIMITED
+ * システム名：LiveTalkAzureTTSSample
+ * 概要      ：Zinrai FAQ 連携サンプルアプリ
+*/
+////using NAudio.Wave;
+using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LiveTalkZinraiFAQSample
+{
+    class Program
+    {
+        static FileCollaboration FileInterface;
+        ////static BlockingCollection<byte[]> AudioQueue = new BlockingCollection<byte[]>();
+        static CancellationTokenSource TokenSource = new CancellationTokenSource();
+        const string IDTag = " ";
+        const string WakeupKeyword = "Zinraiさん：";
+
+        static void Main(string[] args)
+        {
+            var model = new Models.ZinraiFAQModel();
+            var param = new string[]
+            {
+                System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "LiveTalkOutput.csv"),
+                System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "Zinrai.txt"),
+            };
+            if (args.Length >= 1)
+            {
+                param[0] = args[0];
+            }
+            if (args.Length >= 2)
+            {
+                param[1] = args[1];
+            }
+            Console.WriteLine("InputCSVFileName  :" + param[0]);
+            Console.WriteLine("OutputTextFileName:" + param[1]);
+            FileInterface = new FileCollaboration(param[0], param[1]);
+
+            // ファイル入力(LiveTalk常時ファイル出力からの入力)
+            FileInterface.RemoteMessageReceived += async (s) =>
+            {
+                var reg = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+                var items = reg.Split(s);
+                var name = "\"" + System.IO.Path.GetFileNameWithoutExtension(param[1]).ToUpper() + "\"";
+
+                Console.WriteLine(">>>>>>>");
+                if (items[2].IndexOf(IDTag) == 1 && items[1] == name)
+                {
+                    // 自メッセージ出力分なので無視
+                }
+                else if (items[2].IndexOf(WakeupKeyword) == 1)
+                {
+                    // Zinrai FAQ問い合わせなのでZinrai FAQを呼び出す
+                    // LiveTalkで「じんらいさん」を「Zinraiさん：」で単語登録しておくこと
+                    Console.WriteLine("DateTime:" + items[0]);
+                    Console.WriteLine("Speaker:" + items[1]);
+                    Console.WriteLine("Speech contents:" + items[2]);
+                    Console.WriteLine("Translate content:" + items[3]);
+
+                    //Zinrai連携
+                    var question = items[2].Substring(WakeupKeyword.Length + 1, items[2].Length - WakeupKeyword.Length - 2);
+                    var answer = await model.GetAnswer(question);
+                    if (!string.IsNullOrEmpty(answer))
+                    {
+                        var answers = answer.Split('\n');
+                        foreach (var item in answers)
+                        {
+                            if (!string.IsNullOrEmpty(item))
+                            {
+                                FileInterface.SendMessage(IDTag + item);
+                            }
+                        }
+                    }
+                    ////(byte[] waveData, string errorMessage) = await model.TextToSpeechAsync(items[3] == "\"\"" ? items[2] : items[3]);
+                    ////if (waveData != null)
+                    ////{
+                    ////    // 音声合成キューにエントリ
+                    ////    AudioQueue.Add(waveData);
+                    ////}
+                    ////else
+                    ////{
+                    ////    // エラーメッセージ表示
+                    ////    Console.WriteLine(errorMessage);
+                    ////}
+                }
+            };
+
+            ////// 音声合成キュー処理
+            ////Task.Factory.StartNew(() =>
+            ////{
+            ////    while (true)
+            ////    {
+            ////        // 音声合成の再生
+            ////        if (AudioQueue.TryTake(out byte[] data, -1, TokenSource.Token))
+            ////        {
+            ////            using (var ms = new MemoryStream(data))
+            ////            {
+            ////                using (var audio = new WaveFileReader(ms))
+            ////                {
+            ////                    using (var outputDevice = new WaveOutEvent())
+            ////                    {
+            ////                        outputDevice.Init(audio);
+            ////                        outputDevice.Play();
+            ////                        while (outputDevice.PlaybackState == PlaybackState.Playing)
+            ////                        {
+            ////                            Thread.Sleep(1000);
+            ////                        }
+            ////                    }
+            ////                }
+            ////            }
+            ////        }
+            ////    }
+            ////});
+
+            // ファイル監視開始
+            if (System.IO.File.Exists(param[0]))
+            {
+                System.IO.File.Delete(param[0]);
+            }
+            FileInterface.WatchFileSart();
+
+            // 処理終了待ち
+            var message = Console.ReadLine();
+
+            // ファイル監視終了
+            TokenSource.Cancel(true);
+            FileInterface.WatchFileStop();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # LiveTalkZinraiFAQSample
-LiveTalkの発話内容をZinraiFAQと連携してFAQ検索を行い、その結果をLiveTalkに表示するサンプル
+LiveTalk常時ファイル出力で出力したテキストを、Zinrai FAQ検索 をFAQを検索し、その結果をLiveTalk画面に表示するサンプルです。  
+本サンプルコードは、.NET Core 2.2で作成しています。コードレベルでは.NET Framework 4.6と互換性があります。
+
+![Process](https://github.com/FujitsuSSL-LiveTalk/LiveTalkZinraiFAQSample/blob/images/README.png)
+
+# サンプルコードの動き
+本サンプルでは、「Zinraiさん：」と呼び掛けて発話した行をZirani FAQ検索に渡して結果を表示します。サンプルコード動作を簡単に説明すると次のような動作をします。  
+1. LiveTalkで音声認識した結果がファイルに出力されるので、それを自動的に読込み、「Zinraiさん：」から始まる行であれば、Zirani FAQ検索 APIを呼び出します。
+2. Zirani FAQ検索から戻ってきたテキストをLiveTalkが常時ファイル入力として監視しているファイルに出力します。
+3. LiveTalkが常時ファイル出力に出力されたFAQ検索結果を画面に表示します。このときユーザー名は該当ファイル名となります。
+※ LiveTalk連携した他のLiveTalk端末からの発話も対象となり、結果もLiveTalk連携しているすべての端末に表示されます。
+
+
+# 事前準備
+1. [Zinrai FAQ検索 API](https://www.fujitsu.com/jp/solutions/business-technology/ai/ai-zinrai/services/platform/faq-search/index.html)の申込を行い、Zinrai FAQ検索 APIが有効なclient_idとclient_secretを入手します。
+2. client_idとclient_secretをサンプルコードに指定します。
+3. インターネットとの接続がPROXY経由の場合、PROXYサーバーや認証情報を設定してください。
+4. LiveTalkで、デスクトップに常時ファイル出力を「LiveTalkOutput.csv」、常時ファイル入力を「Zirai.txt」として指定してください。
+
+
+# 連絡事項
+本ソースコードは、LiveTalkの保守サポート範囲に含まれません。  
+頂いたissueについては、必ずしも返信できない場合があります。  
+LiveTalkそのものに関するご質問は、公式WEBサイトのお問い合わせ窓口からご連絡ください。
